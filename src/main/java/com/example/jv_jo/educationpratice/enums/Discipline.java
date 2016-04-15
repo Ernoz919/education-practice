09514dae02f0d97d647de8a52f4d7938ff75edde
João Victor <victorjohn919@gmail.com>
2016-04-15 18:20:47 -0300

    Criação de classes para pergunta e resposta

---
+++ src/main/java/com/example/jv_jo/educationpratice/enums/Discipline.java
@@ -0,0 +1,7 @@
+package com.example.jv_jo.educationpratice.enums;
+
+/**
+ * Created by jv_jo on 13/04/2016.
+ */
+public enum Discipline {
+}
---
+++ src/main/java/com/example/jv_jo/educationpratice/models/Question.java
@@ -0,0 +1,7 @@
+package com.example.jv_jo.educationpratice.models;
+
+/**
+ * Created by jv_jo on 13/04/2016.
+ */
+public class Question {
+}
